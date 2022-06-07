25813c1101c794a564a95255ae205c96b86babdf
Ramrai12 <102636380+Ramrai12@users.noreply.github.com>
2022-06-07 14:53:29 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -14,7 +14,7 @@ if "@" in master_user:
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/c70635cc28eec97a43f40.jpg",
+        photo=f"https://te.legra.ph/file/2a1c5660c3a8253e100e9.jpg",
         caption=f"""**𝐓𝐡𝐢𝐬 𝐈𝐬 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 🥀𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐌𝐮𝐬𝐢𝐜 𝐁𝐨𝐭 𝐑𝐮𝐧 𝐎𝐧 𝐏𝐫𝐢𝐯𝐚𝐭𝐞 🥀 𝐕𝐩𝐬 💫𝐒𝐞𝐫𝐯𝐞𝐫 🌎 𝐅𝐞𝐞𝐥 ❤️ 𝐇𝐢𝐠𝐡 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐮𝐬𝐢𝐜 🎧 𝐈𝐧 𝐕𝐜**""",
     reply_markup=InlineKeyboardMarkup(
             [
@@ -24,15 +24,15 @@ async def start_(client: Client, message: Message):
                ],
                 [
                     InlineKeyboardButton(
-                        "Repo ✨", url=f"https://github.com/LEGENDARY-OS/MUSIC-BOT")
-               ], 
-                [
-                    InlineKeyboardButton(
-                        "👨‍💻 Support", url=f"https://t.me/LegendBot_AI")
+                        "Repo ✨", url=f"NtG fUcK OfF AuR sUNo PAHLi me NIKlo yha GAAND na MARWAYA jaye ( WARNING LADKI HO TO BHAAG JAO NAHI TO CHUT CHOD DI JAYEGI MAST TARIKE SE ........... )  ")
                ],
                 [
                     InlineKeyboardButton(
-                        "💝 Help 💝", url=f"https://t.me/LegendBot_OP")
+                        "👨‍💻 Support", url=f"https://t.me/studynsn")
+               ],
+                [
+                    InlineKeyboardButton(
+                        "💝 Help 💝", url=f"https://t.me/studynsn")
                 ]
                 
            ]
@@ -42,13 +42,13 @@ async def start_(client: Client, message: Message):
 @Client.on_message(command(["repo"]) & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/11a149401351a322dc1bc.jpg",
+        photo=f"https://te.legra.ph/file/2a1c5660c3a8253e100e9.jpg",
         caption=f"""""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "💥 Repo 💞", url=f"https://github.com/LEGENDARY-OS/MUSIC-BOT")
+                        "💥 Repo 💞" NtG FuCk oFf")
                 ]
             ]
         ),
